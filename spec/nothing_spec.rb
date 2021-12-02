2afc3fcb67fee0a6756ff7335d67cafec46c43d7
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-02 18:46:40 +0800

    Delete spec directory

--- spec/nothing_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe ActiveNothing::Nothing do
-  describe '#build' do
-    it 'returns new instance of Nothing' do
-      expect(described_class.build).to be_a(described_class)
-    end
-  end
-end
